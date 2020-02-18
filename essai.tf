4729fddbebed9d1cfa571ece417e3112d12fb98d
Ms-27 <ms@juliver.com>
2020-02-18 10:26:04 +0100

    Comments added

--- essai.tf
+++ essai.tf
@@ -1,9 +1,14 @@
+# Connexion au provider, ici Amazon
 provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
     region     = "${var.region}"
 }
 
+# Création d'une vm VM1
+# elle effectue une commande à la création qui écrit son adresse ip dans un fichier sur l'hôte
+# on détermine les security group dans lesquelles la vm est
+# elle est créée après la création du bucket
 resource "aws_instance" "VM1" {
     ami             = "ami-007fae589fdf6e955"
     instance_type   = "t2.micro"
@@ -18,12 +23,13 @@ resource "aws_instance" "VM1" {
     depends_on      = ["aws_s3_bucket.example"]
 }
 
-# Répertoire partagé, équivalent d'un volume kubernetes
+# Répertoire partagé, avec un nom unique (mondial)
 resource "aws_s3_bucket" "example" {
   bucket = "terraform-getting-started-guide-obo"
   acl    = "private"
 }
 
+# Création de le vm VM2
 resource "aws_instance" "VM2" {
     ami             = "ami-007fae589fdf6e955"
     instance_type   = "t2.micro"
@@ -31,6 +37,8 @@ resource "aws_instance" "VM2" {
     security_groups = ["ssh", "default", "${aws_security_group.web.name}"]
 }
 
+# Création d'un groupe de sécurité "web", qui ouvre le port 8080,
+# uniquement à l'ip 195.68.64.154 (ip connexion unique EQL)
 resource "aws_security_group" "web" {
     name        = "web"
     description = "acces sur le port 8080"
